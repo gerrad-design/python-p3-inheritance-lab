56f8d3c7954d8f281b5ef4c43705f959c0cc3835
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-19 11:38:48 +0300

    initial commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -2,9 +2,12 @@
 
 from user import User
 
-import random
+# teacher.py
+from user import User
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]
     def teach(self):
-        pass
+        return self.knowledge[0] 
